99fc71c674a0550c9522b4e4e99a7d5891c16ad2
Warlock <joel.climbs@gmail.com>
2018-09-19 22:05:28 -0700

    hello world

--- main.js
+++ main.js
@@ -10,22 +10,24 @@
 //   body.append(cover);
 // }
 
-$(document).ready(function(){
+// $(document).ready(function(){
  
-  chrome.browserAction.onClicked.addListener(function(tab) {
-    $.ajax({
-        type: "GET",
-        url: "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=Pizza&callback=?",
-        contentType: "application/json; charset=utf-8",
-        async: false,
-        dataType: "json",
-        success: function (data, textStatus, jqXHR) {
-            console.log(data);
-        },
-        error: function (errorMessage) {
-        }
-    });
-  });
+//   chrome.browserAction.onClicked.addListener(function(tab) {
+//     $.ajax({
+//         type: "GET",
+//         url: "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=Pizza&callback=?",
+//         contentType: "application/json; charset=utf-8",
+//         async: false,
+//         dataType: "json",
+//         success: function (data, textStatus, jqXHR) {
+//             alert(data);
+//         },
+//         error: function (errorMessage) {
+//         }
+//     });
+//   });
+// });
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+  alert('Hi Lee, this is your worst nightmare...');
 });
-
-
